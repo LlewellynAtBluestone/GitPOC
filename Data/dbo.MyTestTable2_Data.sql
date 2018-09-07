7145dd14386352f4c83876b6c871b87245564842
llewellyn Egerton <llewellyn.egerton@bluestone.com.au>
2018-09-07 10:27:20 +1000

    Add another static filed

--- Data/dbo.MyTestTable2_Data.sql
+++ Data/dbo.MyTestTable2_Data.sql
@@ -1,4 +1,7 @@
 SET IDENTITY_INSERT [dbo].[MyTestTable2] ON
+INSERT INTO [dbo].[MyTestTable2] ([PrimaryKey2], [Col4], [Col5], [Col6]) VALUES (4, N'ert       ', N'more data ', 3)
+SET IDENTITY_INSERT [dbo].[MyTestTable2] OFF
+SET IDENTITY_INSERT [dbo].[MyTestTable2] ON
 INSERT INTO [dbo].[MyTestTable2] ([PrimaryKey2], [Col4], [Col5], [Col6]) VALUES (1, N'a         ', N'b         ', 1)
 INSERT INTO [dbo].[MyTestTable2] ([PrimaryKey2], [Col4], [Col5], [Col6]) VALUES (2, N'c         ', N'd         ', 2)
 INSERT INTO [dbo].[MyTestTable2] ([PrimaryKey2], [Col4], [Col5], [Col6]) VALUES (3, N'v         ', N'b         ', 2)
